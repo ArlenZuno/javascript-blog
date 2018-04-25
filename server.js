f057f431fb794baed596663f1d1b91a3a9bdbd3e
ArlenZuno <arlenzuno@gmail.com>
2018-04-25 14:23:20 -0700

    testing to delete post

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const port = process.argv[2] || 8080;
-const bodyParser = require('body-parser')
+const bodyParser = require("body-parser");
 
 let blogs = [
   {
@@ -26,6 +26,9 @@ app.set("view engine", "ejs");
 
 app.use(express.static("public"));
 
+// Set the bodyParser to receive input from forms
+app.use(bodyParser.urlencoded({ extended: false }));
+
 app.get("/blog", (req, res) => {
   res.render("pages/blog", { blogs: blogs });
 });
@@ -34,23 +37,41 @@ app.get("/blog/:blogid", (req, res) => {
   let blogid = req.params.blogid;
 
   for (let i = 0; i < blogs.length; i++) {
-      if (blogid === blogs[i].id) {
-          res.render('pages/post', {blog: blogs[i]})
-      }
-  } 
-  res.send(404)
+    if (blogid === blogs[i].id) {
+      res.render("pages/post", { blog: blogs[i] });
+    }
+  }
+  res.send(404);
 });
 
+// REST API "POST" - will add a new blog post (CREATE)
 app.post("/blog", (req, res) => {
-  res.send("Creating a new post");
+  // Push the new blog post in blog array
+  blogs.push({
+    id: `blog-title-${blogs.length + 1}`,
+    title: req.body.title,
+    author: req.body.author,
+    summary: req.body.summary,
+    content: req.body.content,
+    created: new Date()
+  });
+  res.render("pages/blog", { blogs: blogs });
 });
 
-app.post ('/addpost', (req, res) => {
-    res.render('pages/addPost')
-})
+app.get("/addpost", (req, res) => {
+  res.render("pages/addPost");
+});
 
 app.delete("/blog/:blogid", (req, res) => {
-  res.send("Deleting blog post");
+  let blogid = req.params.blogid;
+
+  for (let i = 0; i < blogs.length; i++) {
+    if (blogid === blogs[i].id) {
+        blogs.slice(i, 1)
+        break
+    }
+  }
+  res.render('pages/blog', {blogs: blogs})
 });
 
 app.listen(port, () => {
