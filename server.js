f54a80f491390881313b35514437bc5f424e8e21
ArlenZuno <arlenzuno@gmail.com>
2018-04-25 13:39:24 -0700

    completed part 3

--- server.js
+++ server.js
@@ -1,28 +1,53 @@
-const express = require('express')
-const app = express()
-const port = process.argv[2] || 8080
+const express = require("express");
+const app = express();
+const port = process.argv[2] || 8080;
 
-app.set('view engine', 'ejs')
+let blogs = [
+  {
+    id: "blog-title-1",
+    title: "Blog Title 1",
+    summary: "This is a blog summary.",
+    content: "This is the blog content",
+    author: "John Smith",
+    created: "February 5, 2018"
+  },
+  {
+    id: "blog-title-2",
+    title: "Blog Title 2",
+    summary: "This is a blog summary.",
+    content: "This is the blog content",
+    author: "John Smith",
+    created: "February 6, 2018"
+  }
+];
 
-app.use(express.static('public'))
+app.set("view engine", "ejs");
 
-app.get('/blog', (req, res) => {
-    res.render('pages/blog')
-})
+app.use(express.static("public"));
 
-app.get('/blog/:blogid', (req, res) => {
-    let blogid = Number(req.params.blogid)
-    res.send(blogid)
-})
+app.get("/blog", (req, res) => {
+  res.render("pages/blog", { blogs: blogs });
+});
 
-app.post('/blog', (req, res) => {
-    res.send('Creating a new post')
-})
+app.get("/blog/:blogid", (req, res) => {
+  let blogid = req.params.blogid;
 
-app.delete('/blog/:blogid', (req, res) => {
-    res.send('Deleting blog post')
-})
+  for (let i = 0; i < blogs.length; i++) {
+      if (blogid === blogs[i].id) {
+          res.render('pages/post', {blog: blogs[i]})
+      }
+  } 
+  res.send(404)
+});
+
+app.post("/blog", (req, res) => {
+  res.send("Creating a new post");
+});
+
+app.delete("/blog/:blogid", (req, res) => {
+  res.send("Deleting blog post");
+});
 
 app.listen(port, () => {
-    console.log(`listening on ${port}`)
-})
+  console.log(`listening on ${port}`);
+});
