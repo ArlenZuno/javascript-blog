34b37fa43327b307b5ff54521979464f9b555981
ArlenZuno <arlenzuno@gmail.com>
2018-04-25 15:14:20 -0700

    part 5 complete

--- server.js
+++ server.js
@@ -2,32 +2,7 @@ const express = require("express");
 const app = express();
 const port = process.argv[2] || 8080;
 const bodyParser = require("body-parser");
-const fs = require('fs')
-
-let blogs = [
-  {
-    id: "blog-title-1",
-    title: "Blog Title 1",
-    summary: "This is a blog summary.",
-    content: "This is the blog content",
-    author: "John Smith",
-    created: "February 5, 2018"
-  },
-  {
-    id: "blog-title-2",
-    title: "Blog Title 2",
-    summary: "This is a blog summary.",
-    content: "This is the blog content",
-    author: "John Smith",
-    created: "February 6, 2018"
-  }
-];
-
-let blogsJson = JSON.stringify(blogs)
-
-fs.writeFile('blog-data.json', blogsJson, (err) => {
-    console.log(err)
-})
+const fs = require("fs");
 
 app.set("view engine", "ejs");
 
@@ -37,12 +12,15 @@ app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get("/blog", (req, res) => {
-  res.render("pages/blog", {blogs});
+  let blogsData = fs.readFileSync("blog-data.json");
+  let blogs = JSON.parse(blogsData);
+  res.render("pages/blog", { blogs });
 });
 
 app.get("/blog/:blogid", (req, res) => {
   let blogid = req.params.blogid;
-
+  let blogsData = fs.readFileSync("blog-data.json");
+  let blogs = JSON.parse(blogsData);
   for (let i = 0; i < blogs.length; i++) {
     if (blogid === blogs[i].id) {
       res.render("pages/post", { blog: blogs[i] });
@@ -54,15 +32,22 @@ app.get("/blog/:blogid", (req, res) => {
 // REST API "POST" - will add a new blog post (CREATE)
 app.post("/blog", (req, res) => {
   // Push the new blog post in blog array
-  blogs.push({
+  let blogsData = fs.readFileSync("blog-data.json");
+  let blogs = JSON.parse(blogsData);
+  
+  let blog = {
     id: `blog-title-${blogs.length + 1}`,
     title: req.body.title,
     author: req.body.author,
     summary: req.body.summary,
     content: req.body.content,
     created: new Date()
-  });
-  res.render("pages/blog", {blogs});
+  }
+
+  blogs.push(blog)
+  fs.writeFileSync('blog-data.json', JSON.stringify(blogs))
+
+  res.render("pages/blog", { blogs });
 });
 
 app.get("/addpost", (req, res) => {
@@ -74,11 +59,11 @@ app.delete("/blog/:blogid", (req, res) => {
 
   for (let i = 0; i < blogs.length; i++) {
     if (blogid === blogs[i].id) {
-        blogs.splice(i, 1)
-        break
+      blogs.splice(i, 1);
+      break;
     }
   }
-  res.render('pages/blog', {blogs})
+  res.render("pages/blog", { blogs });
 });
 
 app.listen(port, () => {
